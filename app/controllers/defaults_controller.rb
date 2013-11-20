7b3c2236c7b21fe0e280310340b484ba48f3e744
obliviusm <obliviusm@gmail.com>
2013-11-20 21:26:25 +0200

    minor changes

--- app/controllers/defaults_controller.rb
+++ app/controllers/defaults_controller.rb
@@ -3,7 +3,8 @@ class DefaultsController < ApplicationController
 		@default = Default.find(params[:id])
 		@default.update(default_params)
 		#redirect_to( ("admin/" + @default.table_name.to_s), notice: 'Default was successfully updated')
-		redirect_to :controller => 'admin', :action => 'article'
+		redirect_to :controller => 'admin', :action => @default.table_name.to_s, 
+		:notice => "Default article successfully changed"
 	end
 
 	def default_params
